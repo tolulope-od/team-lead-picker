6c36ba7a2dc2ff737911ea377d2d16d1ead1ccdb
PelumiAlesh <aleshinloyelukmony@gmail.com>
2019-07-20 14:16:15 +0100

    UI

---
+++ UI/index.js
@@ -0,0 +1,68 @@
+async function CreateTableFromJSON() {
+ const res = await fetch('https://team-lead-picker.herokuapp.com/api/v1/team/leaders');
+ const { data } = await res.json();
+ 
+   var myBooks = await data.map((obj) => {
+     console.log(obj);
+   return {
+     'WEEK': `WEEK ${obj.week}`,
+     'TEAM LEAD': obj.teamLead,
+     'QUALITY ASSURANCE': obj.qualityAssurance
+   }
+  });
+  
+
+
+  const currentWeek = myBooks[myBooks.length - 1];
+  const tl = document.getElementById('tl')
+  const qa = document.getElementById('qa')
+  const wk = document.getElementById('week');
+
+  tl.innerText = currentWeek["TEAM LEAD"];
+  qa.innerText = currentWeek["QUALITY ASSURANCE"];
+  wk.innerText = currentWeek["WEEK"];
+
+  var col = [];
+  for (var i = 0; i < myBooks.length; i++) {
+      for (var key in myBooks[i]) {
+          if (col.indexOf(key) === -1) {
+              col.push(key);
+          }
+      }
+  }
+
+
+  var table = document.createElement("table");
+
+
+
+  var tr = table.insertRow(-1);                  
+
+  for (var i = 0; i < col.length; i++) {
+      var th = document.createElement("th");     
+      th.innerHTML = col[i];
+      tr.appendChild(th);
+  }
+
+
+  for (var i = 0; i < myBooks.length; i++) {
+
+      tr = table.insertRow(-1);
+
+      for (var j = 0; j < col.length; j++) {
+          var tabCell = tr.insertCell(-1);
+          tabCell.innerHTML = myBooks[i][col[j]];
+      }
+  }
+
+
+  var divContainer = document.getElementById("showData");
+  divContainer.innerHTML = "";
+  divContainer.appendChild(table);
+}
+
+  CreateTableFromJSON()
+
+
+
+
